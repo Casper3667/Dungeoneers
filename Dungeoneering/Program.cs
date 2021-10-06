3843f42dd0b97c2d7a32e9a7b2c0fa256fc7a568
TGW <mc-casper@hotmail.dk>
2021-10-06 10:56:03 +0200

    Connection test

--- Dungeoneering/Program.cs
+++ Dungeoneering/Program.cs
@@ -7,6 +7,9 @@ namespace Dungeoneering
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //ahufy
+            //Nikolaj
+            //Casper
         }
     }
 }
