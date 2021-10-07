af529859f1a2c32a9e47d35bc8029a60bf3bc7c0
TGW <mc-casper@hotmail.dk>
2021-10-07 12:28:59 +0200

    Adds the first part of enemy

---
+++ Dungeoneering/Characters/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using _Defines;
+
+namespace Dungeon
+{
+    internal class Enemy : Character
+    {
+        public Enemy(string _name, _Defines._Defines.EnemyDamage _damage, _Defines._Defines.EnemyHP _hp, _Defines._Defines.EnemyArmor _armor)
+        {
+            name = _name;
+            damage = (int)_damage;
+            hp = (int)_hp;
+            armor = (int)_armor;
+        }
+
+        public void Attack(Character target)
+        {
+            target.TakeDamage(damage);
+        }
+    }
+}
---
+++ Dungeoneering/Helpers/_Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _Defines
+{
+    public static class _Helper
+    {
+        public static bool CheckHP(int hp)
+        {
+            if (hp <= 0)
+                return false;
+            else
+                return true;
+        }
+
+
+    }
+}
